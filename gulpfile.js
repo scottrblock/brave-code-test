1adf55bd430bf13fee31f103bc682a9e48e07b2d
Scott Block <scottrblock@gmail.com>
2016-09-16 02:51:52 -0400

    initial display and functionality

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,6 @@ gulp.task('scripts', () => {
   return gulp.src('app/scripts/**/*.js')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe($.babel())
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({stream: true}));
